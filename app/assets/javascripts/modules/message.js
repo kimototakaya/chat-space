2aeeeed3a233b70f254783630ef7ae2bf5ac51d3
kimototakaya <independensdey239@gmail.com>
2020-07-24 12:11:23 +0900

    unicorn

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       let html = 
-        `<div class="MessageBox" data-message-id=${message.id}>
+        `<div class="message-box" data-message-id=${message.id}>
           <div class="message-box">
             <div class="message-name">
               ${message.user_name}
@@ -20,7 +20,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox" data-message-id=${message.id}>
+      `<div class="messageBox" data-message-id=${message.id}>
         <div class="message-box">
           <div class="message-name">
             ${message.user_name}
